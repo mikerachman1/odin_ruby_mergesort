0301d509bc1938203f37588aa05304ad709de5cc
Mike Rachman <mikerachman1@gmail.com>
2022-06-16 14:00:43 -0400

    Set up base caseand recursive splitting of array until single element arrays are made

---
+++ mergesort.rb
@@ -0,0 +1,14 @@
+array = [6, 5, 2, 4, 1, 3]
+
+def mergesort(array)
+  return if array.length < 2 #guard clause for single element arrays & empty arrays
+
+  left = array.select { |element| array.index(element) < (array.length / 2) }
+  p left
+  right = array.select { |element| array.index(element) >= (array.length / 2) }
+  p right
+  mergesort(left)
+  mergesort(right)
+end
+
+mergesort(array)
